40f11c91383de32579869da9cc5274ec5ad4ba35
BabyBo <tc10131@hotmail.com>
2023-11-03 23:11:15 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	engine := html.New("./views", ".html")
+	// Reload the templates on each render, good for development
+	engine.Reload(true) // Optional. Default: false
+	// Debug will print each template that is parsed, good for debugging
+	engine.Debug(true) // Optional. Default: false
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Static("/", "./wwwroot")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+	log.Fatal(app.Listen(":3000"))
+}
