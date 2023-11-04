ec041b4baab180574e752be97265a4b010844173
BabyBo Chen <tc10131@hotmail.com>
2023-11-04 19:18:42 +0800

    index

---
+++ wwwroot/scripts/index.js
@@ -0,0 +1,21 @@
+/// <reference path="../libs/tabulator-tables/tabulator.min.js"/>
+let fake = [
+    {
+        name: "foo",
+        gender: "male",
+        col: "black",
+    },
+];
+
+var table = new Tabulator("#example-table", {
+    data: fake,
+    height: "311px",
+    columns: [
+        { title: "Name", field: "name" },
+        { title: "Progress", field: "progress", sorter: "number" },
+        { title: "Gender", field: "gender" },
+        { title: "Rating", field: "rating" },
+        { title: "Favourite Color", field: "col" },
+        { title: "Date Of Birth", field: "dob", hozAlign: "center" },
+    ],
+});
